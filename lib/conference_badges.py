d27ca91e1fecc93398d8975796eed255892a6810
sdbarlow <sethdbarlow@gmail.com>
2023-03-14 15:58:33 -0600

    all tests pass

--- lib/conference_badges.py
+++ lib/conference_badges.py
@@ -1,11 +1,24 @@
 def badge_maker(name):
-    return None
+    badge = (f"Hello, my name is {name}.")
+    return badge
 
 def batch_badge_creator(names):
-    return None
+    badge_batch = []
+    for name in names:
+        badge_batch.append(f"Hello, my name is {name}.")
+    return badge_batch
 
 def assign_rooms(names):
-    return None
+    badge_rooms = []
+    int=1
+    for name in names:
+        badge_rooms.append(f"Hello, {name}! You'll be assigned to room {int}!")
+        int += 1
+    return badge_rooms
 
 def printer(names):
-    return None
+    for badge in batch_badge_creator(names):
+        print(badge)
+    
+    for assignment in assign_rooms(names):
+        print(assignment)
